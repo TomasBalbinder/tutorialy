e4fe2433e8507f2682b382c687108146fa4275f2
TomasB <tomasbalbinder@gmail.com>
2019-11-10 14:12:30 +0100

    vypočet povrchu a objemu čtverce

---
+++ 02/vypocet obsahu obvodu.py
@@ -0,0 +1,6 @@
+# výpočet povrchu a obvodu čtverce 
+
+povrch = int(input("Zadej stranu čtverce pro výpočet povrchu: " ))
+objem = int(input("Zadej stranu čtverce pro výpočet objemu: " ))
+print("Povrch čtverce se stranou", povrch, " je", 6 * povrch**2, "cm2")
+print("Povrch čtverce se stranou", objem, " je", objem**3, "cm3")
